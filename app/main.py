0818702239bc0be2430b8c5428bf56e8daa0b726
Vamp1899 <67709006+Vamp1899@users.noreply.github.com>
2022-06-18 22:36:28 +0530

    imported routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI ,Response ,status ,HTTPException, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import routers
 from routers import post, user, auth, vote
-#Used for CORS in Web
+"""#Used for CORS in Web
 #fetch('http://localhost:8000 ').then(res => res.json()).then(console.log)
 #This will generate table if we don't have alembic setted up from scratch
-#models.Base.metadata.create_all(bind=engine)
-
+#models.Base.metadata.create_all(bind=engine)"""
 app = FastAPI()
 origins = ["*"]
 
@@ -17,12 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from pydantic import BaseModel
-
-
-
-
-
+#from pydantic import BaseModel
 
 #Only one function per URL
 my_posts = [{"title":"title of post 1","content":"content of post 1","id":1},
@@ -37,16 +32,14 @@ def find_index_post(id):
     for i , p in enumerate(my_posts):
         if p['id'] == id:
             return i
-
-
-
 #path route app are same thing
 #app Decorator in this case converts it from simple function to API path operation
 
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
-app.include_router(vote.router)
+
+app.include_router(routers.user.router)
+app.include_router(routers.post.router)
+app.include_router(routers.auth.router)
+app.include_router(routers.vote.router)
 
 @app.get("/")
 def homepage():
